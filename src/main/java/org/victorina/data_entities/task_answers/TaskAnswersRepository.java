0aaaa1d7daf5af4acdfb2a4a0b98a39a672a8545
DanLys <lyskin-2013@mail.ru>
2021-03-09 18:52:27 +0300

    add anotations

--- src/main/java/org/victorina/data_entities/task_answers/TaskAnswersRepository.java
+++ src/main/java/org/victorina/data_entities/task_answers/TaskAnswersRepository.java
@@ -1,12 +1,14 @@
 package org.victorina.data_entities.task_answers;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 /**
  * Created by Danil Lyskin at 21:50 08.03.2021
  */
+@Repository
 public interface TaskAnswersRepository extends JpaRepository<TaskAnswers, Long> {
 
     Optional<TaskAnswers> findAllTaskAnswersByTaskId(Long taskId);
--- src/main/java/org/victorina/data_entities/task_preview/TaskPreviewRepository.java
+++ src/main/java/org/victorina/data_entities/task_preview/TaskPreviewRepository.java
@@ -1,9 +1,11 @@
 package org.victorina.data_entities.task_preview;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by Danil Lyskin at 21:50 08.03.2021
  */
+@Repository
 public interface TaskPreviewRepository extends JpaRepository<TaskPreview, Long> {
 }
